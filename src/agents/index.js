1f4d5c9ee312291a423b33280eed390a6e087a3c
itaico82 <89577356+itaico82@users.noreply.github.com>
2025-03-25 09:43:15 +0200

    Add agents index file

---
+++ src/agents/index.js
@@ -0,0 +1,11 @@
+const BaseAgent = require('./baseAgent');
+const OrchestratorAgent = require('./orchestratorAgent');
+const ValidatorAgent = require('./validatorAgent');
+const ConstructionAnalyzerAgent = require('./constructionAnalyzerAgent');
+
+module.exports = {
+    BaseAgent,
+    OrchestratorAgent,
+    ValidatorAgent,
+    ConstructionAnalyzerAgent
+};
